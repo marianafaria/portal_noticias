893d32ef6614884770f322c7a6d25af55f3787c4
Mariana Faria <marianafaria03@hotmail.com>
2021-04-06 21:47:40 -0300

    Conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
-
-var home = require('./app/routes/home');
-home(app);
-
-var formulario = require('./app/routes/formulario_inclusao_noticia');
-formulario(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var home = require('./app/routes/home')(app);
+var formulario = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
     console.log('Sucesso');
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,13 @@
+var database = require('../../config/database');
+
 module.exports = function (app) {
+
+    var connection = database();
+
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+
+        connection.query('SELECT * FROM noticias', function(erro, result) {
+            res.render("noticias/noticias", {noticias : result});
+        });    
     });
 };
