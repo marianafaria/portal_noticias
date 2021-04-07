2712f37ab0ae20be62fc9b60af1f34e0641d45b7
Mariana Faria <marianafaria03@hotmail.com>
2021-04-06 21:04:06 -0300

    reestruturando aplicação

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.set('view engine', 'ejs');
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
 
-app.get('/', function(req, res){
-    res.render("home/index");
-});
+var home = require('./app/routes/home');
+home(app);
 
-app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
-});
+var formulario = require('./app/routes/formulario_inclusao_noticia');
+formulario(app);
 
 app.listen(3000, function(){
     console.log('Sucesso');
